09dc35e37e229536ba0d58e25dbfe8c00a8d7d26
JungHyukJin <hyukjin4976@naver.com>
2020-03-13 23:01:58 +0900

    close update

--- x_selfPractice/JS-Practice/modalPractice.js
+++ x_selfPractice/JS-Practice/modalPractice.js
@@ -1,7 +1,7 @@
 const openButton = document.getElementById("open");
 const modal = document.querySelector(".modal");
-const overlay = modal.querySelector(".overlay");
-const closeBtn = modal.querySelector("button");
+const overlay = modal.querySelector(".modal__overlay");
+const closeBtn = document.getElementById("close");
 
 const openModal = () => {
   modal.classList.remove("hidden");
@@ -13,3 +13,4 @@ const closeModal = () => {
 
 openButton.addEventListener("click", openModal);
 closeBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
