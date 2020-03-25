82193b518d8465e570631498f35f9b0078a86213
JungHyukJin <hyukjin4976@naver.com>
2020-03-25 20:32:30 +0900

    login page!

--- x_selfPractice/Multicampus6th-Final/main.js
+++ x_selfPractice/Multicampus6th-Final/main.js
@@ -2,7 +2,7 @@
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.2)";
-  document.getElementById("main__header").style.backgroundColor = "white"
+  document.getElementById("main__header").style.backgroundColor = "white";
 }
 
 // 햄버거 메뉴바 닫기
