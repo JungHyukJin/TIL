905a9cb0cd02b647ab27d07d6321f60dc24fb581
JungHyukJin <hyukjin4976@naver.com>
2020-03-27 21:26:51 +0900

    1. 코드 리뷰를 위해 코드 정리. 2. 몇가지 색상 변경. 3. 직접 만든 로고 추가.

--- x_selfPractice/Multicampus6th-Final/main.js
+++ x_selfPractice/Multicampus6th-Final/main.js
@@ -1,12 +1,15 @@
 // 햄버거 메뉴바 열기
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.2)";
-  document.getElementById("main__header").style.backgroundColor = "white";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.3)";
+  document.getElementById("main__header").style.backgroundColor = "#fdfdfd";
+  document.getElementById("login-button").style.opacity = "0.6";
 }
 
 // 햄버거 메뉴바 닫기
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.body.style.backgroundColor = "white";
+  document.body.style.backgroundColor = "#fdfdfd";
+  document.getElementById("login-button").style.opacity = "1";
+
 }
