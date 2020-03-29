b71bd81f858593d5204230c841148b0f251d802f
JungHyukJin <hyukjin4976@naver.com>
2020-03-29 18:36:37 +0900

    update

--- x_selfPractice/Multicampus6th-Final/main.js
+++ x_selfPractice/Multicampus6th-Final/main.js
@@ -1,15 +1,10 @@
 // 햄버거 메뉴바 열기
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.3)";
-  document.getElementById("main__header").style.backgroundColor = "#fdfdfd";
-  document.getElementById("login-button").style.opacity = "0.6";
+  document.querySelector('body').style.background = "#cccccc";
+  document.querySelector(".sidenav").style.width = "250px";
 }
-
 // 햄버거 메뉴바 닫기
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.body.style.backgroundColor = "#fdfdfd";
-  document.getElementById("login-button").style.opacity = "1";
-
+  document.querySelector(".sidenav").style.width = "0";
+  document.querySelector('body').style.background = "white";
 }
