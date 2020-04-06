b6fa87ffc9ee0e321994964eda3879eee21d5a43
JungHyukJin <hyukjin4976@naver.com>
2020-04-06 22:17:44 +0900

    1. 메인 이미지 추가. 2. 반응형 이미지 변경 및 축소 추가. 3. 햄버거 네비바 클릭 시 이미지 blur효과 추가

---
+++ x_selfPractice/Multicampus6th-Final/README.md
@@ -0,0 +1,10 @@
+# Multicampus 6th, Final Project
+
+1. 프로젝트 기간: 2020.03.10 ~ 2020.04.10
+2. 
+
+- 팀 멤버 (Team members) :
+  - 권윤옥(yunok), 김수연(sooyeon), 이선주(seonju), 
+  - 장경석(janggyungseok), 전현진(hyeonjin), 정혁진(hyukjin)
+
+
--- x_selfPractice/Multicampus6th-Final/js/main.js
+++ x_selfPractice/Multicampus6th-Final/js/main.js
@@ -1,10 +1,14 @@
 // 햄버거 메뉴바 열기
 function openNav() {
-  document.querySelector("body").style.background = "#ecf0f1";
+  document.querySelector("body").style.backgroundColor = "#ecf0f1";
   document.querySelector(".sidenav").style.width = "250px";
+  document.getElementById("main__bg-one").style.filter = "blur(2px)";
+  document.getElementById("main__bg-two").style.filter = "blur(2px)";
 }
 // 햄버거 메뉴바 닫기
 function closeNav() {
-  document.querySelector("body").style.background = "#fdfdfd";
+  document.querySelector("body").style.backgroundColor = "#fdfdfd";
   document.querySelector(".sidenav").style.width = "0";
+  document.getElementById("main__bg-one").style.filter = "blur(0px)";
+  document.getElementById("main__bg-two").style.filter = "blur(0px)";
 }
