3db20248792c3620184cfb6f30b4388d2920829c
JungHyukJin <hyukjin4976@naver.com>
2020-04-08 22:12:43 +0900

    1. 오버레이 효과 추가 2. 결과 페이지 설계

--- x_selfPractice/Multicampus6th-Final/js/main.js
+++ x_selfPractice/Multicampus6th-Final/js/main.js
@@ -1,14 +1,11 @@
 // 햄버거 메뉴바 열기
 function openNav() {
   document.querySelector(".sidenav").style.width = "250px";
-  document.querySelector("body").style.backgroundColor = "#ecf0f1";
-  document.getElementById("main__bg-one").style.filter = "blur(2px)";
-  document.getElementById("main__bg-two").style.filter = "blur(2px)";
+  document.getElementById("bg").style.display = "block";
 }
+
 // 햄버거 메뉴바 닫기
 function closeNav() {
   document.querySelector(".sidenav").style.width = "0";
-  document.querySelector("body").style.backgroundColor = "#fdfdfd";
-  document.getElementById("main__bg-one").style.filter = "blur(0px)";
-  document.getElementById("main__bg-two").style.filter = "blur(0px)";
+  document.getElementById("bg").style.display = "none";
 }
