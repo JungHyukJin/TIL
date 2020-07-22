a338c2160d6e8f4e56dc1d7b3a814ff404694f3d
JungHyukJin <hyukjin4976@naver.com>
2020-07-22 23:15:05 +0900

    JS Note commit & initial commit of Portfolio

--- 7.Spring/ScheduleGuide.zip_expanded/src/main/java/com/Schedule/msg/dao/IMemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Schedule.msg.dao;
-import java.util.Map;
-
-import com.Schedule.msg.model.MemberVO;
-
-public interface IMemberRepository {
-	void insertMember(MemberVO mem);
-	void checkMember(MemberVO mem);
-}
