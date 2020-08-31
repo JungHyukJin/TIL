8d68fae5e0c1f263647df90f616792905636fb90
JungHyukJin <hyukjin4976@naver.com>
2020-08-31 16:33:05 +0900

    react note update

--- 6.React/react-tutorial/begin-react/src/App.js
+++ 6.React/react-tutorial/begin-react/src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import Hello from './Hello';
+import './App.css';
 
 function App() {
+  const name = "REACT";
+  const style = {
+    // background-color
+    // - 는 CamelCase를 사용한다.
+    backgroundColor: "black",
+    color: 'aqua',
+    fontSize: 24, // 단위를 사용하지 않으면 자동으로 기본 단위인 px사용.
+    padding: "1rem" // 만약 단위를 따로 설정해주고 싶다면, 문자열로 넣는다.
+  }
+
   return (
-    <div>
+    <>
       <Hello />
-      <Hello />
-      <Hello />
-    </div>
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
+      {/* 블라블라 */}
+    </>
   );
 }
 
