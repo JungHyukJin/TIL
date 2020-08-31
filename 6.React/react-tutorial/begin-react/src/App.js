75b2a9beadf45e24490790d81b6999794bbe6242
JungHyukJin <hyukjin4976@naver.com>
2020-08-31 15:22:20 +0900

    react tutorial

---
+++ 6.React/react-tutorial/begin-react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hello from './Hello';
+
+function App() {
+  return (
+    <div>
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ 6.React/react-tutorial/begin-react/src/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// 컴포넌트 이름은 대문자로 시작
+// 모든곳에 ; 세미콜론은 자유다
+function Hello() {
+  //
+  return <div>안녕하세요!!!</div>
+  // 위의 <div>태그는 html이 아니라 jsx라고 한다.
+}
+
+export default Hello;
+// 헬로 컴포넌트를 만들어서 내보내주겠다.
