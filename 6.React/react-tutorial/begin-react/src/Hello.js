151b1afa1ec423a69113d28653957331e4f71138
JungHyukJin <hyukjin4976@naver.com>
2020-09-20 22:28:09 +0900

    react note update

--- 6.React/react-tutorial/begin-react/src/Hello.js
+++ 6.React/react-tutorial/begin-react/src/Hello.js
@@ -1,22 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Hello({ color, name, isSpecial }) {
-  //
-  return (
-    <div style={{
-      color
-    }}>
-      {isSpecial && <b>***</b>}
-      {/* 단순히 숨기고 보여줄 때는 &&연산자를 사용하고, */}
-      {/* 삼항연산자는 다른 결과를 보여줄 때 사용한다. */}
+class Hello extends Component {
+  static defaultProps = {
+    name: '이름없음',
+  };
 
-      안녕하세요!!! {name}
-    </div>
-  );
-}
-
-Hello.defaultProps = {
-  name: "이름없음"
+  render() {
+    const { color, isSpecial, name } = this.props;
+    return (
+      <div style={{ color }}>
+        {isSpecial && <b>*</b>}
+        안녕하세요{name}
+      </div>
+    );
+  }
 }
 
 export default Hello;
