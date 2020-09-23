b8b1689d7d307704d7ebb0a496090894e2ea247e
JungHyukJin <hyukjin4976@naver.com>
2020-09-23 16:59:51 +0900

    react component styling update

--- 6.React/React_Component_Styling/styling-with-sass/src/App.js
+++ 6.React/React_Component_Styling/styling-with-sass/src/App.js
@@ -6,7 +6,49 @@ function App() {
   return (
     <div className="App">
       <div className="buttons">
-        <Button>Button</Button>
+        <Button size="large"> Button </Button>
+        <Button> Button </Button>
+        <Button size="small"> Button </Button>
+      </div>
+      <div className="buttons">
+        <Button color="gray" size="large">
+          Button
+        </Button>
+        <Button color="gray"> Button </Button>
+        <Button color="gray" size="small">
+          Button
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button color="pink" size="large">
+          Button
+        </Button>
+        <Button color="pink"> Button </Button>
+        <Button color="pink" size="small">
+          Button
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" outline={true}>
+          Button
+        </Button>
+        <Button color="gray" outline>
+          Button
+        </Button>
+        <Button color="pink" size="small" outline>
+          Button
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" fullWidth={true}>
+          Button
+        </Button>
+        <Button size="large" color="gray" fullWidth>
+          Button
+        </Button>
+        <Button size="large" color="pink" fullWidth>
+          Button
+        </Button>
       </div>
     </div>
   );
--- 6.React/React_Component_Styling/styling-with-sass/src/components/Button.js
+++ 6.React/React_Component_Styling/styling-with-sass/src/components/Button.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import './Button.scss';
+import classNames from 'classnames';
 
-function Button({ children }) {
-  return <button className="Button"> {children} </button>;
+function Button({ children, size, color, outline, fullWidth }) {
+  return (
+    <button
+      className={classNames('Button', size, color, { outline, fullWidth })}
+      // props로받아온 outline과 fullWidth값이 트루일 때만, 전체 클래스네임에 포함된다.
+      // 즉, 값이 트루일때만 적용된다.
+    >
+      {children}
+    </button>
+  );
 }
+
+Button.defaultProps = {
+  size: 'medium',
+  color: 'blue',
+};
 
 export default Button;
