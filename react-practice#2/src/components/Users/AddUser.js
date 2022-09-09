9d3ecccd68a45ddd8e8d86a7c83cdd8fe64b26d6
JungHyukJin <hyukjin4976@naver.com>
2022-09-09 15:45:27 +0900

    React: practice#2 done

---
+++ react-practice#2/src/components/Users/AddUser.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+
+import classes from "./AddUser.module.css";
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import ErrorModal from '../UI/ErrorModal';
+
+const AddUser = (props) => {
+  const [enteredUserName, setEnteredUserName] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState(false);
+
+  const addUSerHandler = (event) => {
+    event.preventDefault();
+    
+    if(enteredUserName.trim().length === 0 || enteredAge.trim().length === 0){
+      setError({
+        title: 'Invalid input',
+        message: 'Please enter a valid name and age(non-empty values).',
+      });
+      return;
+    };
+
+    if(+enteredAge < 1){
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age.",
+      });
+      return;
+    }
+
+    props.onAddUser(enteredUserName, enteredAge);
+    setEnteredUserName("");
+    setEnteredAge("");
+  };
+
+  const userNameChangeHandler = (event) => {
+    setEnteredUserName(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+  const errorHandler = props => {
+    setError(false);
+  };
+  return (
+    // Card태그는 html 내장된 태그가 아니고 커스텀 태그이기 때문에
+    // props를 넘겨 클래스를 적용해줘야 함.
+    <div>
+      {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
+      <Card className={classes.input}>
+        <form onSubmit={addUSerHandler}>
+          <label htmlFor="username">User Name</label>
+          <input
+            id="username"
+            type="text"
+            value={enteredUserName}
+            onChange={userNameChangeHandler}
+          />
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            id="age"
+            type="number"
+            value={enteredAge}
+            onChange={ageChangeHandler}
+          />
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
+  );
+};
+
+export default AddUser;
---
+++ react-practice#2/src/components/Users/UsersList.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import classes from "./UsersList.module.css";
+import Card from "../UI/Card";
+
+const UserList = (props) => {
+  return (
+    <Card className={classes.users}>
+      <ul>
+        {props.users.map((user) => (
+          <li key={user.id}>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+};
+
+export default UserList;
