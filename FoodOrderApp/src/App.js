18f1b09cbd14a32cf1565997b437bb29dd98549c
JungHyukJin <hyukjin4976@naver.com>
2022-09-11 16:55:04 +0900

    update

--- FoodOrderApp/src/App.js
+++ FoodOrderApp/src/App.js
@@ -1,8 +1,17 @@
+import { Fragment } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
   );
 }
 
---
+++ FoodOrderApp/src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,20 @@
+import CartIcon from "../Cart/Cart";
+import classes from "./HeaderCartButton.module.css";
+
+const HeaderCartButton = (props) => {
+  return (
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>
+        Your Cart
+      </span>
+      <span className={classes.badge}>
+        3
+      </span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
