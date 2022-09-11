b399f9d37df566e77dcdd5110685967b077c9fdf
JungHyukJin <hyukjin4976@naver.com>
2022-09-11 14:59:46 +0900

    Small Project: Food Order App

--- Effects, Reducers, Context/src/App.js
+++ Effects, Reducers, Context/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
@@ -6,14 +6,24 @@ import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
+
+  useEffect(()=>{
+    const storedUserLoggedIn = localStorage.getItem('isLoggedIn');
+    if(storedUserLoggedIn === '1'){
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
