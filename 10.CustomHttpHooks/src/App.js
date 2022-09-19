aef2a25c039dea853920e3951e1e796a52567faf
JungHyukJin <hyukjin4976@naver.com>
2022-09-19 19:52:47 +0900

    update

---
+++ 10.CustomHttpHooks/src/App.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+import useHttp from "./hooks/use-http";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const transformTasks = (tasksObject) => {
+    const loadedTasks = [];
+
+    for (const taskKey in tasksObject) {
+      loadedTasks.push({ id: taskKey, text: tasksObject[taskKey].text });
+    }
+
+    setTasks(loadedTasks);
+  };
+
+  const {
+    isLoading,
+    error,
+    sendRequest: fetchTasks,
+  } = useHttp(
+    {
+      url: "https://usehttp-hooks-default-rtdb.firebaseio.com/tasks.json",
+    },
+    transformTasks
+  );
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
+  return (
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
