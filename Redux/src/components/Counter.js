8c11c7ca89025048efad60f142298c75dae0ca8a
JungHyukJin <hyukjin4976@naver.com>
2022-09-27 15:28:54 +0900

    Redux Study update

--- Redux/src/components/Counter.js
+++ Redux/src/components/Counter.js
@@ -1,12 +1,40 @@
-import classes from './Counter.module.css';
+// 리덕스 훅
+// useSelector: 자동으로 상태의 일부를 선택하게 해준다
+// useDispatch: React컴포넌트 내부에서 Redux로 action을 보낼 때 사용
+import { useSelector, useDispatch } from "react-redux";
+
+import classes from "./Counter.module.css";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  // Redux store에 action을 보낸다
+  const dispatch = useDispatch();
+  // redux에서 관리하는 state값으로 counter값을 가져온다.
+  const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
+
+  const incrementHandler = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+  const increaseHandler = () => {
+    dispatch({ type: "INCREASE", amount: 5 });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "DECREMENT" });
+  };
+
+  const toggleCounterHandler = () => {
+    dispatch({ type: "TOGGLE" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>INCREMENT</button>
+        <button onClick={increaseHandler}>INCREMENT +5</button>
+        <button onClick={decrementHandler}>DECREMENT</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- Redux/src/store/index.js
+++ Redux/src/store/index.js
@@ -1,19 +1,41 @@
-import redux from "redux";
+import {createStore} from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = {counter:0, showCounter:true}
+
+const counterReducer = (state = initialState, action) => {
   if (action.type === "INCREMENT") {
+    // state.counter ++;
+    // return state;
+    // 위 방식으로 해도 동작하지만, redux를 사용할 때 절대로 해서는 안된다.
+    // 절대로 기존의 state를 변경해서는 안된다.
+    
+    // 반드시 아래처럼 새로운 state객체를 변환하여 항상 재정의 해야한다.
     return{
-        counter: state.counter + 1
+        counter: state.counter + 1,
+        showCounter: state.showCounter,
+    };
+  } 
+  if (action.type === "INCREASE") {
+    return{
+        counter: state.counter + action.amount,
+        showCounter: state.showCounter,
     };
   }
-  if (action.type === "INCREMENT") {
+  if (action.type === "DECREMENT") {
     return {
-        counter: state.counter - 1
+        counter: state.counter - 1,
+        showCounter: state.showCounter,
+    };
+  };
+  if (action.type === "TOGGLE") {
+    return {
+        counter: state.counter,
+        showCounter: !state.showCounter,
     };
   };
   return state;
 };
 
-const store = redux.createStore(counterReducer);
+const store = createStore(counterReducer);
 
 export default store;
